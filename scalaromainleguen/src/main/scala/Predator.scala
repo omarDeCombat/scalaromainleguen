80f2c80e0cf5a26ef28fce8dc72b6ffed88bba1c
Romain Le Guen <96078121+omarDeCombat@users.noreply.github.com>
2024-07-04 22:05:05 +0200

    jeux de la vie

--- scalaromainleguen/src/main/scala/Predator.scala
+++
@@ -1,9 +0,0 @@
-package scala
-
-import scala.Main.cellSize
-import scalafx.scene.shape.Rectangle
-
-final case class Predator(x:Int,y:Int,direction:Char){
-
-    
-}
--- scalaromainleguen/src/main/scala/Prey.scala
+++
@@ -1,9 +0,0 @@
-package scala
-
-import scala.Main.cellSize
-import scalafx.scene.shape.Rectangle
-
-final case class Prey(x:Int,y:Int,direction:Char){
-
-    
-}
